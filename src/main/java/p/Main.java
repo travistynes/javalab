181663382db588768bce146e5df4716c3d2c8434
Travis Tynes <ttynes@merchante-solutions.com>
2018-04-23 16:36:13 -0400

    Minor changes.

--- src/main/java/p/Main.java
+++ src/main/java/p/Main.java
@@ -7,6 +7,9 @@ public class Main {
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) throws Exception {
-
+		log.info("Environment:");
+		System.getProperties().forEach((k, v) -> {
+			log.info(k + ": " + v);
+		});
 	}
 }
