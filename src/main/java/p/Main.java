511228e64e526cf4c60de04a33a370eb9cf867ac
Travis Tynes <ttynes@merchante-solutions.com>
2018-04-23 14:11:16 -0400

    Add .gitignore and apply formatting.

--- src/main/java/p/Main.java
+++ src/main/java/p/Main.java
@@ -4,9 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
-    
-    public static void main(String[] args) throws Exception {
-        
-    }
+	private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) throws Exception {
+
+	}
 }
--- src/test/java/p/Tests.java
+++ src/test/java/p/Tests.java
@@ -15,44 +15,64 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Tests {
-    private static final Logger log = LoggerFactory.getLogger(Tests.class);
-    
-    private DataSource ds;
-    
-    @Before
-    public void setup() throws Exception {
-        log.info("Setting up H2 db.");
-        
-        ds = JdbcConnectionPool.create("jdbc:h2:mem:testdb;MODE=Oracle", "sa", "");
-        
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream("sql/create_table_a.sql");
-        String sql = IOUtils.toString(is, "UTF-8");
-        
-        ds.getConnection().prepareStatement(sql).execute();
-    }
-    
-    @After
+
+	private static final Logger log = LoggerFactory.getLogger(Tests.class);
+
+	private DataSource ds;
+
+	@Before
+	public void setup() throws Exception {
+		log.info("Setting up H2 db.");
+
+		ds = JdbcConnectionPool.create("jdbc:h2:mem:testdb;MODE=Oracle", "sa", "");
+
+		InputStream is = this.getClass().getClassLoader().getResourceAsStream("sql/create_table_a.sql");
+		String sql = IOUtils.toString(is, "UTF-8");
+
+		ds.getConnection().prepareStatement(sql).execute();
+	}
+
+	@After
 	public void cleanup() throws Exception {
-        log.info("Cleanup.");
+		log.info("Cleanup.");
 		ds.getConnection().prepareStatement("SHUTDOWN").execute();
 	}
-    
-    @Test
-    public void test1() throws Exception {
-        Connection con = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        
-        try {
-            con = ds.getConnection();
-            ps = con.prepareStatement("select x from a");
-            rs = ps.executeQuery();
-            
-            Assert.assertFalse(rs.next());
-        } finally {
-            try { if(con != null) con.close(); } catch(Exception ex) {}
-            try { if(ps != null) ps.close(); } catch(Exception ex) {}
-            try { if(rs != null) rs.close(); } catch(Exception ex) {}
-        }
-    }
+
+	@Test
+	public void test1() throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = ds.getConnection();
+			ps = con.prepareStatement("select x from a");
+			rs = ps.executeQuery();
+
+			Assert.assertFalse(rs.next());
+		}
+		finally {
+			try {
+				if (con != null) {
+					con.close();
+				}
+			}
+			catch (Exception ex) {
+			}
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+			}
+			catch (Exception ex) {
+			}
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+			}
+			catch (Exception ex) {
+			}
+		}
+	}
 }
