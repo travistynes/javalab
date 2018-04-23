785e3e34836e8fde8dc73962d00e01470c77d6a0
Travis Tynes <ttynes@merchante-solutions.com>
2018-04-23 12:50:09 -0400

    Basic maven build.

--- src/main/java/p/Main.java
+++ src/main/java/p/Main.java
@@ -1,9 +1,10 @@
 package p;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
-    private static final Logger log = Logger.getLogger(Main.class);
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
     
     public static void main(String[] args) throws Exception {
         
