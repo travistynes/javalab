c8c87968f9f209467c32072ad1034ec28438d864
Travis Tynes <travistynes@gmail.com>
2018-10-09 21:34:20 -0400

    Work on adding multiple RabbitMQ endpoints.

--- src/main/java/p/amqp/RabbitManager.java
+++ src/main/java/p/amqp/RabbitManager.java
@@ -7,14 +7,44 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 @Configuration
-//@EnableRabbit
 public class RabbitManager {
 	private static final Logger log = LoggerFactory.getLogger(RabbitManager.class);
 
-	@RabbitListener(queues = "${spring.rabbitmq.queueName}")
+	private final String queueName = "testq";
+
+	@RabbitListener(containerFactory = "localhostContainerFactory", queues = queueName)
 	public void processMessage(String message) {
 		log.info("Got message: " + message);
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "mq.a")
+	public SimpleRabbitListenerContainerFactory localhostContainerFactory(@Qualifier("localhostConnectionFactory") ConnectionFactory connectionFactory) {
+		SimpleRabbitListenerContainerFactory f = new SimpleRabbitListenerContainerFactory();
+		f.setConnectionFactory(connectionFactory);
+
+		return f;	
+	}
+
+	private static class ConnectionFactories {
+		@Bean
+		@ConfigurationProperties(prefix = "mq.a")
+		public ConnectionFactory localhostConnectionFactory() {
+			/*
+			 * This works because CachingConnectionFactory has the matching 
+			 * setters for Spring to inject the properties matching the configuration
+			 * prefix.
+			 */
+			CachingConnectionFactory cf = new CachingConnectionFactory();
+			return cf;
+		}
 	}
 }
