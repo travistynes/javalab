707c78bc55c44b288b493dd5a5ff2d8b1d389179
Travis Tynes <ttynes@merchante-solutions.com>
2018-10-25 15:56:40 -0400

    Add a few comments.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,5 @@
 -- A file named import.sql in the root of the classpath is executed on startup if Hibernate creates the schema from scratch.
+-- This is useful to populate sample data when running on an embedded database.
 -- https://docs.spring.io/spring-boot/docs/current/reference/html/howto-database-initialization.html
 
 insert into person (id, name, age) values(1, 'bob', 10),(2, 'mary', 20);
