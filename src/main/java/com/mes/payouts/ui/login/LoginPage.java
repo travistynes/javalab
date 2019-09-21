3c48c3bd426458cc559295b13f2e5188a90ac284
Travis Tynes <travistynes@gmail.com>
2019-09-20 22:44:36 -0400

    Update

--- src/main/java/com/mes/payouts/ui/login/LoginPage.java
+++ src/main/java/com/mes/payouts/ui/login/LoginPage.java
@@ -5,6 +5,8 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.datepicker.DatePicker;
@@ -17,6 +19,7 @@ import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
 import com.vaadin.flow.component.login.LoginForm;
+import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -53,43 +56,69 @@ public class LoginPage extends VerticalLayout {
 
 	private void createUI() {
 		HorizontalLayout header = new HorizontalLayout();
-		HorizontalLayout content = new HorizontalLayout();
+		VerticalLayout content = new VerticalLayout();
 		HorizontalLayout footer = new HorizontalLayout();
 
 		// Configure layouts
 		this.setSizeFull();
-		this.setPadding(true);
+		this.setPadding(false);
 		this.setSpacing(true);
 
 		header.setWidth("100%");
 		header.setPadding(true);
 		header.setJustifyContentMode(JustifyContentMode.CENTER);
-
-		content.setWidth("100%");
-		content.setFlexGrow(1, content);
-		content.setAlignItems(Alignment.CENTER); // Vertical alignment
-		content.setJustifyContentMode(JustifyContentMode.CENTER); // Horizontal alignment
-
-		footer.setWidth("100%");
-		footer.setPadding(true);
-		footer.setAlignItems(Alignment.CENTER);
-		footer.add(new H3("Footer"));
+		header.addClassName("header");
 		
+		Image headerLogo = new Image("frontend/images/logo_black_transparent.png", "Merchant e-Solutions");
+		headerLogo.addClassName("header-logo");
+		header.add(headerLogo);
+
+		/**
+		 * See flex box alignment:
+		 * https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Aligning_Items_in_a_Flex_Container
+		 */
+		content.setWidth("100%");
+		content.setAlignItems(Alignment.CENTER); // Cross axis
+		content.setJustifyContentMode(JustifyContentMode.EVENLY); // Main axis
+
+		Paragraph p1 = new Paragraph("Login to opt-in to the Faster Funding and Immediate Pre-Funding program.");
+
 		/**
 		 * Add login form.
 		 * To set a custom error message, see setI18n(LoginI18n).
 		 * https://vaadin.com/api/platform/14.0.5/com/vaadin/flow/component/login/LoginForm.html
 		 */
-		//loginForm.addClassName("login-border");
 		loginForm.setForgotPasswordButtonVisible(false);
-		content.getElement().appendChild(loginForm.getElement());
+		//content.getElement().appendChild(loginForm.getElement());
 
 		// Login submit listener
 		loginForm.addLoginListener(event -> login(event));
 
+		footer.setWidth("100%");
+		footer.setPadding(true);
+		footer.setSpacing(false);
+		footer.addClassName("footer");
+		footer.setAlignItems(Alignment.CENTER);
+		footer.setJustifyContentMode(JustifyContentMode.EVENLY);
+
+		Image footerMesLogo = new Image("frontend/images/logo_white.png", "Merchant e-Solutions");
+		footerMesLogo.addClassName("footer-mes-logo");
+
+		Image partnerIcon = new Image("frontend/images/partner_icon.png", "Technology Partner");
+		partnerIcon.addClassName("partner-icon");
+
+		Image footerPayoutsNetworkLogo = new Image("frontend/images/logo_payoutsnetwork.png", "Payouts Network");
+		footerPayoutsNetworkLogo.addClassName("footer-pon-logo");
+
+		footer.add(footerMesLogo, partnerIcon, footerPayoutsNetworkLogo);
+		
 		// Compose layout
 		this.add(header, content, footer);
-		//this.expand(center);
+		content.add(p1, loginForm);
+
+		//this.expand(content);
+		//content.setFlexGrow(1, p1);
+		this.setFlexGrow(1, content);
 	}
 
 	private void login(LoginEvent event) {
@@ -105,7 +134,7 @@ public class LoginPage extends VerticalLayout {
 			securityContext.setAuthentication(auth);
 
 			// Redirect authenticated user
-			UI.getCurrent().navigate(com.mes.payouts.controllers.MerchantSignupPage.class);
+			UI.getCurrent().navigate(LoginSuccess.class);
 		} catch(BadCredentialsException e) {
 			loginForm.setError(true);
 		}
