fc754934bac73a6872f6f66d1db5f3944becc49a
Travis Tynes <travistynes@gmail.com>
2019-11-21 22:52:56 -0500

    Add logback dependency.

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,5 +1,10 @@
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("main");
-	}
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+def log = LoggerFactory.getLogger(Main)
+
+def f = {
+	println "Closure"
 }
+
+f()
