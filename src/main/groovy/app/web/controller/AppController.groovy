e30ac48229236f6067038d8e37a3e443dc0863ed
Travis Tynes <travistynes@gmail.com>
2019-12-01 23:16:50 -0500

    Add jsp.

--- src/main/groovy/app/web/controller/AppController.groovy
+++ src/main/groovy/app/web/controller/AppController.groovy
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/app")
 class AppController {
 	@RequestMapping(value="/{name}", method=RequestMethod.GET)
-	public String hello(@PathVariable(name="name", required=true) String name) {
-		return "Hello, ${name}";
+	public ModelAndView welcome(@PathVariable(name="name", required=true) String name) {
+		ModelAndView model = new ModelAndView("welcome");
+		model.addObject("user", name);
+
+		return model;
 	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,9 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+	<head>
+		<title>Welcome</title>
+	</head>
+	<body>
+		Welcome, ${user}.
+	</body>
+</html>
